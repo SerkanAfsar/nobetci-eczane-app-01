59eeb4b28a0228dae27f2732c7668f9ac94b5ae7
SerkanAfsar <serkan-afsar@hotmail.com>
2025-04-12 18:46:55 +0300

    Analitics Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -28,6 +29,7 @@ export default function RootLayout({
         <Header />
         <main className="block w-full flex-auto">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
